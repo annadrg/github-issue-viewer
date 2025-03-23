e8fafecd98c25177f3c251dc2d68e71886e20fce
Anna de Ruig <joannaderuig@hotmail.com>
2025-03-23 16:05:59 +0100

    add loading state

--- src/components/search-results.tsx
+++ src/components/search-results.tsx
@@ -3,13 +3,30 @@
 import { useInfiniteRepositoryIssues } from "@/lib/hooks";
 import InfiniteScroll from "./infinite-scroll";
 import IssueCard from "./issue-card";
+import { Card, CardContent, CardHeader } from "./ui/card";
+import { Skeleton } from "./ui/skeleton";
 
 export default function SearchResults() {
-  const { data } = useInfiniteRepositoryIssues();
+  const { data, isLoading } = useInfiniteRepositoryIssues();
   const issues = data?.pages.flatMap((page) => page.issues) || [];
 
   return (
     <div className="mt-10 flex flex-col gap-2">
+      {isLoading && (
+        <div className="space-y-4">
+          {[...Array(10).keys()].map((i) => (
+            <Card key={i}>
+              <CardHeader>
+                <Skeleton className="h-6 w-3/4" />
+              </CardHeader>
+              <CardContent>
+                <Skeleton className="mb-2 h-4 w-1/2" />
+                <Skeleton className="h-4 w-1/4" />
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      )}
       {issues.map((issue) => (
         <IssueCard
           {...issue}
